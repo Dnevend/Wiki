530d9ed042d8b0cfdb5c0f37740bb598dbd8c18d
Dnevend <Dnevend@gmail.com>
2023-10-26 11:59:15 +0800

    chore: update config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,12 +14,14 @@ const config = {
   url: 'https://your-docusaurus-test-site.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/wiki',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Dnevend', // Usually your GitHub org/user name.
+  projectName: 'wiki', // Usually your repo name.
+
+  deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
