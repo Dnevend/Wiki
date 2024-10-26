328f80e53b46f0bfc3e2899edfde7c9d598e67ed
Dnevend <Dnevend@gmail.com>
2024-10-26 13:54:54 +0800

    feat: home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 import clsx from 'clsx';
 // import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
@@ -9,24 +9,27 @@ import ZhanXiFuChangLiu from '../../static/img/zxfcl.png';
 import YunWoYiShangLeng from '../../static/img/ywysl.png';
 
 const taglines = [
-  '含哺而熙，鼓腹而游。',
   '恐惧吞蚀灵魂',
-  '一个人应该有能力换尿布、策划侵略、杀猪、给轮船掌舵、设计建筑物、写十四行诗、平衡收支、造墙、接骨、安慰临终之人、写菜单、发布命令、合作、独立工作、解方程、分析新问题、施肥、写电脑程序、做可口的饭菜、有效地战斗、英勇地死去。——罗伯特·海因莱因',
+  '聪明意味着把事情想清楚，愚蠢意味着直接给出结论而逃避思考。',
+  '你不能搞混两样东西，一是你终将胜利的信念——这是你最输不起的；二是训练自己直面残酷现实——无论现实多残酷。',
+  '一个人应该有能力换尿布、策划侵略、杀猪、给轮船掌舵、设计建筑物、写十四行诗、平衡收支、造墙、接骨、安慰临终之人、写菜单、发布命令、合作、独立工作、解方程、分析新问题、施肥、写电脑程序、做可口的饭菜、有效地战斗、英勇地死去。',
 ]
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
+  const [showTagline, setShowTagline] = useState(0);
+
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{taglines[Math.floor(Math.random() * taglines.length)]}</p>
+        <p className="hero__subtitle">
+          “{taglines[showTagline]}”
+        </p>
         <div className={styles.buttons}>
-          {/* <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link> */}
+          <button onClick={() => {
+            setShowTagline(Math.floor(Math.random() * taglines.length));
+          }}>🎲</button>
         </div>
       </div>
 
